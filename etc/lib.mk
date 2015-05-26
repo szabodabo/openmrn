924cb1fbb064ee9f8e78008a76b133eef5cd7e8b
Stuart W. Baker <stuart_w_baker@ymail.com>
2015-05-25 19:47:15 -0500

    cygwin test

--- etc/lib.mk
+++ etc/lib.mk
@@ -45,7 +45,7 @@ endif
 
 ifeq ($(OS),Windows_NT)
 #COMPILE_PREFIX := ccache
-INCLUDES_TMP := $(INCLUDES:-I=)
+INCLUDES_TMP := $(subst -I,,$(INCLUDES))
 INCLUDES = $(foreach mypath,$(INCLUDES_TMP),-I$(shell cygpath -ma $(mypath)))
 else
 COMPILE_PREFIX :=
